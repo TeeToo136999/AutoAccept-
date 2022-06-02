f5abfec6dc2751606a327f39163013838eda4011
TeeToo <91502673+TeeToo136999@users.noreply.github.com>
2022-06-02 23:00:30 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace lolautoaccept
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.autoacceptb = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // autoacceptb
+            // 
+            this.autoacceptb.AutoSize = true;
+            this.autoacceptb.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.4F);
+            this.autoacceptb.Location = new System.Drawing.Point(124, 12);
+            this.autoacceptb.Name = "autoacceptb";
+            this.autoacceptb.Size = new System.Drawing.Size(86, 19);
+            this.autoacceptb.TabIndex = 0;
+            this.autoacceptb.Text = "autoaccept";
+            this.autoacceptb.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 46);
+            this.Controls.Add(this.autoacceptb);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AutoAccept";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox autoacceptb;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoItX3Lib;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace lolautoaccept
+{
+    public partial class Form1 : Form
+    {
+        IntPtr handle;
+        string WINDOW_NAME = "League Of Legends";
+
+        AutoItX3 au3 = new AutoItX3();
+
+        public RECT rect;
+
+        public struct RECT
+        {
+            public int left, top, right, bottom;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string IpClassName, string IpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern bool GetWindowRect(IntPtr hwnd, out RECT IpRect);
+
+        int col;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread AA = new Thread(auto_accept) { IsBackground = true };
+            AA.Start();
+        }
+
+        private void auto_accept()
+        {
+            while (true)
+            {
+                if (autoacceptb.Checked)
+                {
+                    getwindowrectangle();
+                    col = au3.PixelGetColor(rect.left + 584, rect.top + 555);
+                    if (col == 0x1e252A)
+                    {
+                        Thread.Sleep(20);
+                        au3.MouseMove(rect.left + 584, rect.top + 555, 1);
+                        au3.MouseClick("LEFT");
+                        Thread.Sleep(20);
+                        au3.MouseClick("LEFT");
+                        Thread.Sleep(5000);
+                    }
+                }
+                Thread.Sleep(20);
+            }
+        }
+
+        void getwindowrectangle()
+        {
+            handle = FindWindow(null, WINDOW_NAME);
+            GetWindowRect(handle, out rect);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
